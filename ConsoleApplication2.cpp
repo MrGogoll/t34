8ca9775480129657dd77d772fb585b807c9e5ebb
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-03 20:12:11 +0300

    t34

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin >> a;
+	switch (a)
+	{
+	case 1:
+			cout << "matesha" << endl << "english" << endl << "geografish)";
+				break;
+	case 2:
+		cout << "english" << endl << "matesha";
+				break;
+	case 3:
+		cout << "matesha" << endl << "infa";
+					break;
+	case 4:
+		cout << "geografish)";
+						break;
+	case 5:
+		cout << "matesha" << endl << "matesha" << endl << "matesha";
+			break;
+	case 6:
+		cout << "otdih";
+								break;
+	case 7:
+		cout << "otdih";
+									break;
+	default:
+		break;
+	}
+}
